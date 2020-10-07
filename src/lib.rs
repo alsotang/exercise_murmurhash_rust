7854dbdf23c4ba976816b6c58ddf23760b2b2de1
alsotang <alsotang@gmail.com>
2020-10-07 16:04:07 +0800

    refactor variable name

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,29 @@
-use bytes::Buf;
+use bytes::{Buf};
 
-fn murmur_32_scramble(init_k: u32) -> u32 {
-    let mut k = init_k;
-    k = k.wrapping_mul(0xcc9e2d51);
-    k = (k << 15) | (k >> 17);
-    k = k.wrapping_mul(0x1b873593);
+fn murmur_32_scramble(k: u32) -> u32 {
+    let mut k2 = k;
+    k2 = k2.wrapping_mul(0xcc9e2d51);
+    k2 = (k2 << 15) | (k2 >> 17);
+    k2 = k2.wrapping_mul(0x1b873593);
 
-    k
+    k2
 }
 
-pub fn murmurhash(key_init: &[u8], seed: u32) -> u32 {
-    let mut key = key_init;
+pub fn murmurhash(key: &[u8], seed: u32) -> u32 {
+    let mut buf = key;
     let mut h = seed;
-    let len = key.len();
+    let len = buf.len();
 
-
-    while key.remaining() >= 4 {
-        let k = key.get_u32();
+    while buf.remaining() >= 4 {
+        let k = buf.get_u32();
         h ^= murmur_32_scramble(k);
         h = (h << 13) | (h >> 19);
         h = h.wrapping_mul(5).wrapping_add(0xe6546b64);
     }
 
     let mut k: u32 = 0;
-    while key.has_remaining() {
-        k |= key.get_u8() as u32;
+    while buf.has_remaining() {
+        k |= buf.get_u8() as u32;
         k <<= 8;
     }
 
